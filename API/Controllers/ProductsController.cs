bfefb2b7249fe82359ab241e84cad76fcefde09e
Siddartha Mote <siddart14393@mastek.com>
2022-01-31 23:39:38 +0530

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+            // return "this will be a list of products";
+        }
+
+        [HttpGet("(id)")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
